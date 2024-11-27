101f4347f2442a97847691d84e72d78863b826ef
galdoben <galdobenedetta@gmail.com>
2024-11-27 17:23:21 +0100

    fix: array of objects

--- script.js
+++ script.js
@@ -23,3 +23,24 @@ for (i = 0; i < bikes.length; i++){
 
 }
 console.log(bikeLeggera.name)
+
+
+
+const team = [
+    {
+        name: '11 uomini e una gamba',
+        goals: '0',
+        fouls: '0',
+    },
+    {
+        name: 'Atletico ma non troppo',
+        goals: '0',
+        fouls: '0',
+    },
+    {
+        name: '50 sfumature di Gigi',
+        goals: '0',
+        fouls: '0',
+    }
+]
+Math.floor(Math.random() * 11);
